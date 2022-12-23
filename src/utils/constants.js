8729ae93df0742e260b6015bca20d69a519d8574
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-12-23 14:32:22 +0300

    PR 10

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const validationConfig = {
-  formSelector: ".form",
-  inputSelector: ".form__text",
-  submitButtonSelector: ".form__save",
-  inactiveButtonClass: "form__save_inactive",
-  inputErrorClass: "form__text_type_error",
-  errorClass: "form__text-error_active",
-};
